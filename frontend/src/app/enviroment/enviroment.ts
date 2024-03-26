a04adbb7e20d15e4cf98baab5a8d7229eba497ca
Jakub Piórkowski <74377061+purkos@users.noreply.github.com>
2024-03-26 17:56:10 +0100

    Update enviroment.ts

--- frontend/src/app/enviroment/enviroment.ts
+++ frontend/src/app/enviroment/enviroment.ts
@@ -1,6 +1,5 @@
 export const enviroment = {
   production: false,
   apiUrl: "http://localhost:3001",
-  // soccerApiKey: '960b97bec94747d99d88468d701300fc',
   soccerApiUrl: "https://www.thesportsdb.com/api/v1/json/3",
 };
